e8133b7f9dfd8cd53bcb9859e54a6886b4a98c78
Dušan Ferenčík <dusan.ferencik@st.fri.uniza.sk>
2019-01-02 21:31:24 +0100

    Prekopanie sietovej casti

--- net.c
+++ net.c
@@ -5,8 +5,9 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include "defs.h"
+#include <stdint.h>
 #include <pwd.h>
+#include "defs.h"
 
 
 int net_connect(int *sck, int port, char *adr) {
@@ -33,21 +34,84 @@ int net_connect(int *sck, int port, char *adr) {
 	return 1;
 	}
 
+int net_login(int socket){
+	char *buffer;
+	register struct passwd *pw;
+	pw = getpwuid(geteuid());
 
-void printBuff(char* buff, unsigned size){
-	printf("\n\n");
-	for(unsigned i=0; i<size; i++)
-		printf("buffer[%d]: 0x%02X = %3d = \'%c\'\n", i, buff[i], buff[i], buff[i]);
+	buffer = malloc(3+strlen(pw->pw_name));
+
+	*buffer = SERVER_CMD_USER;
+	*(buffer+1) = (uint16_t) strlen(pw->pw_name);
+	strcpy(buffer+2, pw->pw_name);
+	write(socket, buffer, 3+strlen(buffer+3));
+
+	free(buffer);
+
+	char reponse;
+	read(socket, &response, 1);
+
+	return response;
 }
 
+int net_setFileName(int socket, char* filename){
+	char *buffer;
+	buffer = malloc(3 + strlen(filename));
 
-void net_save(void *w){
+	*buffer = SERVER_CMD_FILENAME;
+	*(buffer+1) = (uint16_t) strlen(filename);
+	memcpy(buffer+3, filename, strlen(filename));
+	write(socket, buffer, 3+strlen(buffer+3));
+
+	free(buffer);
+
+	char reponse;
+	read(socket, &response, 1);
+
+	return response;
+}
+
+int net_sendWorld(int socket, void *w){
 	world_t (*world) = w;
 
+	char* buffer = malloc(7);								//minimalna mozna velkost: 1B-CMD + 1B-sirka + 1B-vyska + 2B-celkova dlzka + 2B-pocet mrtvych buniek
+
+	*buffer = SERVER_CMD_SAVE;
+	*(buffer+3) = (uint8_t) world->w;
+	*(buffer+4) = (uint8_t) world->h;
+
+	uint16_t series = 0;
+	char cell_state = 0;
+	uint16_t buff_ptr = 5;
+
+	for(unsigned i=0; i<(world->w * world->h); i++){
+		if(*(world->cells + i) != cell_state){
+			*(buffer + buff_ptr) = series;					//pocet rovnakych posebe iducich buniek
+			buff_ptr += 2;
+			buffer = realloc(buffer, 2 + buff_ptr);
+			series = 1;
+			cell_state = *(world->cells + i);
+		} else
+			series++;
+	}
+	*(buffer + buff_ptr) = series;
+	*(buffer + 1) = (uint16_t) (buff_ptr - 3);				//pocet zmien x2 - vysledna velkost sveta po "komprimacii"
+	
+	write(socket, buffer, buff_ptr + 2);
+
+	free(buffer);
+
+	char reponse;
+	read(socket, &response, 1);
+
+	return response;
+}
+
+void net_save(void *w){
 	char addr[18];
 	int socket;
 
-	printf("Zadaj IP adresu servera [localhost] : ");
+	printf("Zadaj IP adresu servera [localhost] : ");		//pripojenie ku serveru
 	fgets(addr, 18, stdin);
 	addr[strcspn(addr, "\n")] = 0;
 	if(!addr[0])
@@ -57,47 +121,44 @@ void net_save(void *w){
 		sleep(2);
 		return;
 	}
-
 	printf("Pripojenie uspesne\n");
 
-	char *buffer;
-	register struct passwd *pw;
-	pw = getpwuid(geteuid());
-	
-	buffer = malloc(strlen(pw->pw_name));
-	strcpy(buffer, pw->pw_name);
-	write(socket, buffer, strlen(buffer));
 
-	char odpoved;
-	read(socket, &odpoved, 1);
-	if(odpoved == SERVER_USER_OK)
+	char ret = net_login(socket);							//prihlasenie na server
+	if(ret == SERVER_RESP_OK)
 		printf("Pouzivatel \"%s\" prihlaseny\n", buffer);
-	else if(odpoved == SERVER_USER_NEW)
+	else if(ret == SERVER_RESP_NEW)
 		printf("Vytvoreny novy pouzivatel \"%s\"\n", buffer);
 
-	printf("Zadaj nazov suboru: ");
 
-	buffer = realloc(buffer, 30);
-	fgets(buffer+3, 27, stdin);
-	if(*(buffer+3) == 0 || *(buffer+3) == '\n')
-		strcpy(buffer+3, "new.gol");
+	printf("Zadaj nazov suboru: ");							//nazov suboru
+	char filename = malloc(FILENAME_MAX);
+	fgets(filename, FILENAME_MAX, stdin);
+	if(*(filename) == 0 || *(filename) == '\n')
+		strcpy(filename, "new_save");
 
-	unsigned buffSize = strlen(buffer+3) + 3 + world->w*world->h;
-
-	*buffer = world->w;
-	*(buffer+1) = world->h;
-	*(buffer+2) = strlen(buffer+3);
-	
-	buffer = realloc(buffer, buffSize);
-	memcpy(buffer+3+strlen(buffer+3), world->cells, world->w*world->h);
-
-	write(socket, buffer, buffSize);
-	free(buffer);
-
-	read(socket, &odpoved, 1);
-	if(odpoved = SERVER_SAVE_OK){
-		printf("Ukladanie uspesne\n");
-	}else{
-		printf("Chyba pri ukladani\n");
+	ret = net_setFileName(socket, filename);
+	switch(ret){
+		case SERVER_RESP_OK:
+			printf("Vzdialeny subor vytvoreny\n");
+			break;
+		case SERVER_RESP_ERR:
+			printf("Chyba pri vytvarani suboru\n");
+			return;
+			break;
+		case SERVER_RESP_OK:
+			printf("Subor uz existuje, prepisujem!\n");
+			break;
+		default:
+			printf("Neznama chyba!\n");
+			return;
+			break;
 	}
+
+
+	ret = net_sendWorld(socket, w);							//odoslanie aktualneho stavu
+	if(ret = SERVER_RESP_OK)
+		printf("Ukladanie uspesne\n");
+	else
+		printf("Chyba pri ukladani\n");
 }
