432b21fb2065959c7ea80fb2317d3eee1cae1082
Dušan Ferenčík <dusan.ferencik@st.fri.uniza.sk>
2019-01-09 09:06:55 +0100

    Posledne ladenie

--- net.h
+++ net.h
@@ -3,5 +3,12 @@
 
 void net_save(world_t *);
 void net_load(world_t *);
+int net_connect(int *, int, char *);
+void net_close(int);
+int net_rewrite(int);
+int net_login(int, char *);
+int net_setFileName(int, char*);
+int net_sendWorld(int, world_t *);
+void net_readWorld(world_t *, unsigned char *, unsigned);
 
 #endif
